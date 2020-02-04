17619969c16e6e52f8cc34c4159d3fe9d245ff8e
dhxiang <daihuixiang@xiaoyouzi.com>
2020-02-04 15:44:10 +0800

    【update】优化启动页自定义接口实现方式

--- ios/Classes/FlutterMySplashScreenPlugin.m
+++ ios/Classes/FlutterMySplashScreenPlugin.m
@@ -1,13 +1,5 @@
 #import "FlutterMySplashScreenPlugin.h"
  
-@interface MyLaunchWindow : NSObject
-
-+ (void)show;
-
-+ (void)hide;
-
-@end
-
 
 @implementation FlutterMySplashScreenPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -16,63 +8,16 @@
              binaryMessenger:[registrar messenger]];
    FlutterMySplashScreenPlugin* instance = [[FlutterMySplashScreenPlugin alloc] init];
    [registrar addMethodCallDelegate:instance channel:channel];
-    
-   [MyLaunchWindow show];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"show" isEqualToString:call.method]) {
+      [[NSNotificationCenter defaultCenter] postNotificationName:@"flutter_my_splash_screen_show" object:nil];
   } else if ([@"hide" isEqualToString:call.method]) {
-      [MyLaunchWindow hide];
+     [[NSNotificationCenter defaultCenter] postNotificationName:@"flutter_my_splash_screen_hide" object:nil];
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 @end
-
-
-static UIView *_myLaunchiWindow = nil;
-
-@implementation MyLaunchWindow
-
-+ (void)show
-{
-    CGRect windowFrame = [UIScreen mainScreen].bounds;
-    if (UIInterfaceOrientationIsLandscape(
-            [UIApplication sharedApplication].statusBarOrientation)) {
-        windowFrame.size.width = [UIScreen mainScreen].bounds.size.height;
-        windowFrame.size.height = [UIScreen mainScreen].bounds.size.width;
-    }
-
-    UIWindow *lauchWindow = [[UIWindow alloc] initWithFrame:windowFrame];
-    lauchWindow.backgroundColor = [UIColor whiteColor];
-   
-    UIImageView *middleImgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"qidongtu_pic_nvrentong.png"]];
-    middleImgView.center = CGPointMake(CGRectGetWidth(windowFrame)/2.0, CGRectGetHeight(windowFrame)/2.0);
-    [lauchWindow addSubview:middleImgView];
-     
-    UIImageView *bottomImgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"qidongtu_pic_meiyou.png"]];
-    bottomImgView.center = CGPointMake(CGRectGetWidth(windowFrame)/2.0, CGRectGetHeight(windowFrame) - CGRectGetHeight(bottomImgView.bounds)/2.0 - 28);
-    [lauchWindow addSubview:bottomImgView];
-     
-    
-    [UIView animateWithDuration:0.001 animations:^{
-        
-    } completion:^(BOOL finished) {
-        lauchWindow.hidden = NO;
-    }];
-     
-    _myLaunchiWindow = lauchWindow;
-}
-    
-
-+ (void)hide
-{
-    if (_myLaunchiWindow != nil) {
-        [_myLaunchiWindow removeFromSuperview];
-        _myLaunchiWindow = nil;
-    }
-}
  
-
-@end
