9aad979190119e9a87b75b12c2de7d59ba0ed378
dhxiang <daihuixiang@xiaoyouzi.com>
2020-02-05 11:49:12 +0800

    [update]增加静态单例存储注册

--- ios/Classes/FlutterMySplashScreenPlugin.m
+++ ios/Classes/FlutterMySplashScreenPlugin.m
@@ -1,5 +1,6 @@
 #import "FlutterMySplashScreenPlugin.h"
  
+static FlutterMySplashScreenPlugin *_mysPluginInstance = nil;
 
 @implementation FlutterMySplashScreenPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -8,14 +9,16 @@
              binaryMessenger:[registrar messenger]];
    FlutterMySplashScreenPlugin* instance = [[FlutterMySplashScreenPlugin alloc] init];
    [registrar addMethodCallDelegate:instance channel:channel];
+   _mysPluginInstance = instance;
+    [[[UIAlertView alloc] initWithTitle:@"注册channel" message:@"Plugin" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil] show];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"show" isEqualToString:call.method]) {
-      [[[UIAlertView alloc] initWithTitle:@"接收到展示通知" message:@"Plugin" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil] show];
+      [[[UIAlertView alloc] initWithTitle:@"接收到展示通知" message:@"Plugin" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil] show];
       [[NSNotificationCenter defaultCenter] postNotificationName:@"flutter_my_splash_screen_show" object:nil];
   } else if ([@"hide" isEqualToString:call.method]) {
-      [[[UIAlertView alloc] initWithTitle:@"接收到隐藏通知" message:@"Plugin" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil] show];
+      [[[UIAlertView alloc] initWithTitle:@"接收到隐藏通知" message:@"Plugin" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil] show];
      [[NSNotificationCenter defaultCenter] postNotificationName:@"flutter_my_splash_screen_hide" object:nil];
   } else {
     result(FlutterMethodNotImplemented);
