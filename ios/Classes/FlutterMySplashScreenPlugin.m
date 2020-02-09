b609cdf7343fb385e1881857356d93095245ff1e
dhxiang <daihuixiang@xiaoyouzi.com>
2020-02-09 20:26:35 +0800

    【update】还原通知

--- ios/Classes/FlutterMySplashScreenPlugin.m
+++ ios/Classes/FlutterMySplashScreenPlugin.m
@@ -10,13 +10,14 @@
    FlutterMySplashScreenPlugin* instance = [[FlutterMySplashScreenPlugin alloc] init];
    [registrar addMethodCallDelegate:instance channel:channel];
 //   _mysPluginInstance = instance;
+        [[[UIAlertView alloc] initWithTitle:@"注册channel" message:@"Plugin" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil] show];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"show" isEqualToString:call.method]) {
       [[NSNotificationCenter defaultCenter] postNotificationName:@"flutter_my_splash_screen_show" object:nil];
   } else if ([@"hide" isEqualToString:call.method]) {
-    
+    [[[UIAlertView alloc] initWithTitle:@"接收到隐藏通知" message:@"Plugin" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil] show];
      [[NSNotificationCenter defaultCenter] postNotificationName:@"flutter_my_splash_screen_hide" object:nil];
   } else {
     result(FlutterMethodNotImplemented);
